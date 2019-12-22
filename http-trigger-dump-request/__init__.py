8da5650915cc60c9be7fdde83601a9c750e6db37
Ahmad <a-ahhmou@microsoft.com>
2019-12-22 17:56:36 +0200

    Add out param

--- http-trigger-dump-request/__init__.py
+++ http-trigger-dump-request/__init__.py
@@ -2,11 +2,13 @@ import logging
 import json
 import azure.functions as func
 
-def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
+def main(req: func.HttpRequest, msg: func.Out[str], msg2: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     msg.set('Message from http')
-    
+
+    msg2.set('Message from http')
+
     return func.HttpResponse(
         json.dumps({
             'method': req.method,
